abdaf8708379c49cc2b04d14e129903cb4d93641
KJCA <kimjoshuaadvincula@gmail.com>
2013-08-07 16:47:39 +0800

    Round 1 View (data retreival using ajax only)

--- application/views/admin.php
+++ application/views/admin.php
@@ -5,14 +5,18 @@
 <br/>
 <br/>
 <div class='container'>
+	
+	<center>
 
-	<a href="/user/app_config">App Config</a>
-	<a href="/user/add_team">Teams</a>
-	<a href="/user/add_user">Add User</a>
-	<a href="/user/edit_round1">Edit Round1 Questions</a>
-	<a href="/user/edit_round2">Edit Round2 Questions</a>
-	<a href="/user/encoder_round1">Encoder (Round1)</a>
-
+		<a href="/user/app_config"><button class="btn">App Config</button></a>
+		<a href="/user/add_team"><button class="btn">Teams</button></a>
+		<a href="/user/add_user"><button class="btn">Add User</button></a>
+		<a href="/user/edit_badge"><button class="btn">Badges</button></a>
+		<a href="/user/edit_round1"><button class="btn">Edit Round1 Questions</button></a>
+		<a href="/user/edit_round2"><button class="btn">Edit Round2 Questions</button></a>
+		<a href="/user/encoder_round1"><button class="btn">Encoder (Round1)</button></a>
+		
+	</center>
 </div>
 
 <?php
