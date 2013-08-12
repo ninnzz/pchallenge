dc527537579aed09963df257fab5e96b8adbce64
KJCA <kimjoshuaadvincula@gmail.com>
2013-08-12 17:22:08 +0800

    Round1 Timer + fixed too many setIntervals -Mark

--- application/views/round1_view.php
+++ application/views/round1_view.php
@@ -1,4 +1,3 @@
-
 <?php $this->load->view('includes/header2'); ?>
 
 <script src="/js/timer.js"></script>
@@ -47,7 +46,7 @@
 
 <div id="pre"> Papers are something, pens are something. Yehey get 1/4! </div>
 <div id="countdown">countdown</div>
-<div id = "round1Timer">Timer</div><br/><br/><br/>
+<div id = "round1Timer"></div><br/><br/><br/>
 <div id = "newsfeed"></div>
 <div id = "text"></div>
 
@@ -88,7 +87,6 @@
 	}, 1000);
 
 	function update_newsfeed(){
-		setInterval(function(){
     	$.get("/events/latest", function (latest_news) {
     		temp2 =latest_news;
     		var latest_json = eval("(" + latest_news + ")");
@@ -98,10 +96,6 @@
 //	    	var message = json_json=>[0].evnt;
 //          	$("#newsfeed").html(message);
     	});
-      },
-      //The delay before the data is refreshed
-      100);
-		
 	}
 
 	// if round1/state says the state has changed, react accordingly
@@ -126,6 +120,8 @@
 					function(){
 						current_state="round_1";
 						setState(current_state);
+						
+						countdown("round1Timer", 45*60, function(){});
 					}
 				);
 			}
@@ -136,7 +132,6 @@
 	}
 	
 	function update(){
-		setInterval(function(){
     	$.get("/round1/team_score", function (team_scores) {
     		var teamscores_json = eval("(" + team_scores + ")");
 	    	var message = teamscores_json.message+"<br/>";
@@ -147,9 +142,6 @@
 
           	$("#text").html(message);
     	});
-      },
-      //The delay before the data is refreshed
-      100);
 	}
 </script>
 
