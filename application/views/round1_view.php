4f5478564dd8e34e90e21cc62a4ea59e88349d64
KJCA <kimjoshuaadvincula@gmail.com>
2013-09-12 16:43:36 +0800

    carousel fixed round1

--- application/views/round1_view.php
+++ application/views/round1_view.php
@@ -131,22 +131,13 @@
 		<div class="badge_container">
             <div id="badge_slider" class="carousel slide">
                 <div class="carousel-inner"height="150px" width="300px">
-                    <!--div class="laman item" height="100%" width="100%">
-
-                        <div class="badge_pic">
-                            <img src="/img/badge/Collectibles.png" height="85%" width="90%" />
-                        </div>
-                        <div class="badge_title">
-                            Badge Title
-                        </div>
-                        <div class="badge_owner">
-                            Badge Owner
-                        </div>
-
-                    </div-->
+                    <div class="item" height="100%" width="100%">
+                    </div>
+                    <div class="item" height="100%" width="100%" style="margin-top:4%;font-size:500%">
+                            <center><p>BADGES ACQUIRED</p></center>
+                    </div>
                 </div>
             </div>
-
 		</div>
 	</div>
 </div>
@@ -273,8 +264,9 @@ function update_achievements(){
                 badge_title = "Lucky Star";
             }
 
+
             $("#badge_slider > div").append(
-                "<div class='laman item' height='100%' width='100%'>"+
+                "<div class='item' height='100%' width='100%'>"+
                 "<div class='badge_pic'>"+
                 "<img src='"+image_path+"' height='85%' width='90%' />"+
                 "</div>" +
@@ -288,7 +280,7 @@ function update_achievements(){
             );
 
 //          $('#badge_slider').carousel().click();
-            $('#badge_slider').carousel('pause');
+            $('#badge_slider').carousel();
             $("#badge_slider").carousel({interval:2000});
             /*$('body').append(
                 "<link rel='stylesheet' type='text/css' href='/css/bootstrap.min.css'>"+
@@ -345,7 +337,7 @@ function state_reactor(){
 
             //"countdown"==id of div to present countdown on
             // 10 = seconds
-            countdown("countdown", 10,
+            countdown("countdown", 2,
                 function(){
                     $("#countdown").html("Round 1");
                     setTimeout(function(){
@@ -459,9 +451,9 @@ function startGameLoop(){
     }, 100);
 
 //    $('#badge_slider').carousel();
+    $('#badge_slider').carousel(function(){$('#slider').carousel().click();});
 
     //makes the carousel start automatically, without needing a manual mouse click
-    $('#slider').carousel().click();
 //    $('#badge_slider').carousel().click();
 }
 
